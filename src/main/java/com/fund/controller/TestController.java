a35ff1eff33c13b9a5b2ec6be15a76908a7c865f
Administrator <asdasd>
2021-08-09 02:31:24 +0800

    mytest

--- src/main/java/com/fund/controller/TestController.java
+++ src/main/java/com/fund/controller/TestController.java
@@ -1,9 +1,12 @@
 package com.fund.controller;
 
 import com.fund.service.FundService;
+import com.fund.vo.Fund;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TestController {
@@ -11,6 +14,13 @@ public class TestController {
 
     @Autowired
     private FundService fundService;
+
+
+    @RequestMapping("search")
+    public List<Fund> search(String fundName) {
+       return fundService.search(fundName);
+
+    }
 
     @RequestMapping("crawl")
     public String save() {
--- src/main/java/com/fund/service/FundService.java
+++ src/main/java/com/fund/service/FundService.java
@@ -10,19 +10,28 @@ import com.fund.vo.Fund;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.Aggregations;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.aggregation.AggregatedPage;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.data.elasticsearch.core.query.SearchQuery;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Stream;
 
@@ -35,6 +44,10 @@ public class FundService {
     private HttpComponent httpComponent;
 
 
+    @Autowired
+    private ElasticsearchTemplate elasticsearchTemplate;
+
+
     public static final String fundListUrl = "https://fundapi.eastmoney.com/fundtradenew.aspx?ft=%s&sc=1n&st=desc" +
             "&pi=%s" +
             "&pn=%s" +
@@ -44,6 +57,37 @@ public class FundService {
 
     @Autowired
     private FundDao fundDao;
+
+
+    public List<Fund> search(String fundName) {
+//        // 创建一个查询条件对象
+////        BoolQueryBuilder queryBuilder = QueryBuilders.boolQuery();
+////        // 拼接查询条件
+////        //queryBuilder.should(QueryBuilders.termQuery("fundName", fundName));
+////        queryBuilder.should(QueryBuilders.termsQuery("manager", "施红俊"));
+////        // 创建聚合查询条件
+////        // 创建查询对象
+////        SearchQuery build = new NativeSearchQueryBuilder()
+////                .withQuery(queryBuilder) //添加查询条件
+////                .withPageable(PageRequest.of(0, 10)) //符合查询条件的文档分页（不是聚合的分页）
+////                .build();
+////
+////        // 执行查询
+////        AggregatedPage<Fund> testEntities = elasticsearchTemplate.queryForPage(build, Fund.class);
+////
+////        List<Fund> fundList = testEntities.getContent();
+////
+////        return fundList;
+
+        BoolQueryBuilder queryBuilder = QueryBuilders.boolQuery();
+        queryBuilder.must(QueryBuilders.termQuery("fundName", fundName));
+        queryBuilder.must(QueryBuilders.matchQuery("manager", "吴逸"));
+        SearchQuery searchQuery = new NativeSearchQueryBuilder()
+                .withQuery(queryBuilder)
+                .withPageable(PageRequest.of(0, 10)).build();
+        return elasticsearchTemplate.queryForList(searchQuery, Fund.class);
+
+    }
 
     public void showFund() {
         Map<String, String> headMap = Maps.newHashMap();
@@ -60,14 +104,8 @@ public class FundService {
         Map maps = (Map) JSON.parse(result.substring(result.indexOf("{"), result.length() - 1));
         int allPages = Integer.parseInt(String.valueOf(maps.get("allPages")));
         int pageNum = Integer.parseInt(String.valueOf(maps.get("pageNum")));
-        int allRecords = Integer.parseInt(String.valueOf(maps.get("allRecords")));
-       /*Stream.of(allPages).forEach(e -> {
-            log.info("==========================" + e);
-            saveFund(type, e, pageNum, headMap);
-            //log.info(JSON.toJSONString(fundList));
-        });*/
+        //int allRecords = Integer.parseInt(String.valueOf(maps.get("allRecords")));
         for (int i = 1; i <= allPages; i++) {
-            log.info("==========================" + i);
             saveFund(type, i, pageNum, headMap);
         }
     }
@@ -133,7 +171,7 @@ public class FundService {
         Elements trElements = element.getElementsByTag(HtmlElementEnum.P.name());
         Element elementFirst = trElements.get(0);
         if (elementFirst != null) {
-            Elements tdElements = elementFirst.getElementsByTag( HtmlElementEnum.LABEL.name());
+            Elements tdElements = elementFirst.getElementsByTag(HtmlElementEnum.LABEL.name());
             for (Element tempElement : tdElements) {
                 String html = HtmlUtil.removeBlank(tempElement.html());
                 if (html.contains("成立日期")) {
